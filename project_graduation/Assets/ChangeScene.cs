4bb998884f5e99206df8140767173b5ca531efa4
Hemirich <hjinshan0813@gmail.com>
2022-09-04 12:13:34 +0800

    a

--- project_graduation/Assets/ChangeScene.cs
+++ project_graduation/Assets/ChangeScene.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
+    
     public void MoveToScene(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
     }
-   
+    
 }
+
+
