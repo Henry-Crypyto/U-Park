20fd847380f550b789b1f6c3a04ed1ef1a5b22dd
Hemirich <hjinshan0813@gmail.com>
2022-11-26 22:39:17 +0800

    Finished QRcode

---
+++ Assets/Scripts/FindSlot/QRcode/QRcode.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZXing;
+using TMPro;
+using UnityEngine.UI;
+public class QRcode : MonoBehaviour
+{
+    [SerializeField]
+    private RawImage rawImageBackGround;
+    [SerializeField]
+    private AspectRatioFitter aspectRatioFitter;
+    [SerializeField]
+    private TMPro.TMP_Text textOut;
+    [SerializeField]
+    private RectTransform scanZone;
+
+    private bool CameraStat;
+    private WebCamTexture cameratexture;
+    
+    
+    void Start()
+    {
+        SetUpCamera();
+    }
+
+    
+    void Update()
+    {
+        UpdateCameraRender();
+        Scan();
+    }
+    
+    private void SetUpCamera(){
+        WebCamDevice[] devices=WebCamTexture.devices;
+        if(devices.Length==0){
+            CameraStat=false;
+            return;
+        }
+        for(int i=0;i<devices.Length;i++){
+            if(devices[i].isFrontFacing==false){
+                cameratexture=new WebCamTexture(devices[i].name,(int)scanZone.rect.width,(int)scanZone.rect.height);
+            }
+        }
+        cameratexture.Play();
+        rawImageBackGround.texture=cameratexture;
+        CameraStat=true;
+    }
+
+    private void UpdateCameraRender(){
+        if(CameraStat==false){
+            return;
+        }
+        float ratio=(float)cameratexture.width/(float)cameratexture.height;
+        aspectRatioFitter.aspectRatio=ratio;
+
+        int orientation=-cameratexture.videoRotationAngle;
+        rawImageBackGround.rectTransform.localEulerAngles=new Vector3(0,0,orientation);
+    }
+
+    public void OnClickScan(){
+        Scan();
+    }
+
+    private void Scan(){
+        try{
+             IBarcodeReader barcodeReader =new BarcodeReader();
+             Result result=barcodeReader.Decode(cameratexture.GetPixels32(),cameratexture.width,cameratexture.height);
+             if(result!=null){
+                textOut.text=result.Text;
+             }
+             else{
+                textOut.text="Failed to read QRcode";
+             }
+        }
+        catch{
+            textOut.text="Failed in try";
+        }
+    }
+}
